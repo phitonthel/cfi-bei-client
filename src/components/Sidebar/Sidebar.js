b42d8a24cb43a364c4a652ae6f2564a83d5e3720
phitonthel <thephitonthel@gmail.com>
2023-10-19 01:30:57 -0400

    add dropdown sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -17,18 +17,57 @@
 */
 import React, { Component } from "react";
 import { useLocation, NavLink } from "react-router-dom";
-
-import { Nav } from "react-bootstrap";
+import { Nav, NavDropdown } from "react-bootstrap";
+import styled from 'styled-components';
 
 import logo from "assets/img/reactlogo.png";
+
+const StyledNavDropdown = styled(NavDropdown)`
+    /* default styles for NavDropdown */
+
+    ${({ isActive }) => isActive && `
+        background-color: #404040; /* or your desired highlight color */
+        /* any other styles you want to apply when it's active */
+    `}
+`;
+
+const StyledDropdownItem = styled(NavLink)`
+    padding: 15px 40px;  // Increased padding for better spacing
+    color: #616161;
+    transition: all 0.2s;
+    display: flex;       // Flex layout for better alignment
+    align-items: center; // Vertically center the text and icon
+    border-bottom: 1px solid #404040;  // Divider between items
+
+    &:last-child {
+        border-bottom: none; // Remove border from the last item
+    }
+
+    &:hover {
+        background-color: #404040;
+        color: #ffffff;
+    }
+
+    i {
+        margin-right: 10px;
+    }
+`;
 
 function Sidebar({ color, image, routes }) {
   const level = localStorage.getItem("level");
 
   const location = useLocation();
 
-  const activeRoute = (routeName) => {
-    return location.pathname == routeName ? "active" : "";
+  const activeRoute = (routeName, children = []) => {
+    // Check if the current pathname matches the parent route
+    if (location.pathname === routeName) return "active";
+
+    // Check if the current pathname matches any of the children routes
+    for (const child of children) {
+      if (location.pathname === child.layout + child.path) return "active";
+    }
+
+    return "";
   };
 
   return (
@@ -61,30 +100,55 @@ function Sidebar({ color, image, routes }) {
             const isAccessValid = !level ? true : prop.access.includes(level)
             const isHiddenFromSidebar = prop.hidden
 
-            if (
-              !isRedirect &&
-              !isHiddenFromSidebar &&
-              isAccessValid
-            )
-              return (
-                <li
-                  className={
-                    prop.upgrade
-                      ? "active active-pro"
-                      : activeRoute(prop.layout + prop.path)
-                  }
-                  key={key}
-                >
-                  <NavLink
-                    to={prop.layout + prop.path}
-                    className="nav-link"
-                    activeClassName="active"
+            if (!isRedirect && !isHiddenFromSidebar && isAccessValid) {
+              if (prop.children) {
+                return (
+                  <StyledNavDropdown
+                    className="custom-nav-dropdown"
+                    title={
+                      <>
+                        {/* <i className={prop.icon} style={{ marginRight: '10px', fontSize: '20px' }} /> */}
+                        <span style={{ fontSize: '18px' }}>{prop.name}</span>
+                      </>
+                    }
+                    id={`nav-dropdown-${key}`}
+                    key={key}
+                    isActive={activeRoute(prop.layout + prop.path, prop.children) === "active"}
                   >
-                    <i className={prop.icon} />
-                    <p>{prop.name}</p>
-                  </NavLink>
-                </li>
-              );
+                    {prop.children.map((child, childKey) => (
+                      <StyledDropdownItem
+                        to={child.layout + child.path}
+                        activeClassName="active"
+                        key={childKey}
+                      >
+                        <i className={child.icon} />
+                        {child.name}
+                      </StyledDropdownItem>
+                    ))}
+                  </StyledNavDropdown>
+                )
+              } else {
+                return (
+                  <li
+                    className={
+                      prop.upgrade
+                        ? "active active-pro"
+                        : activeRoute(prop.layout + prop.path)
+                    }
+                    key={key}
+                  >
+                    <NavLink
+                      to={prop.layout + prop.path}
+                      className="nav-link"
+                      activeClassName="active"
+                    >
+                      <i className={prop.icon} />
+                      <p>{prop.name}</p>
+                    </NavLink>
+                  </li>
+                )
+              }
+            }
             return null;
           })}
         </Nav>
